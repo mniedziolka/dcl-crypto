e87100d185f4ffc08741846ff6b19648f6a75d53
niedziol <michalekniedziolka@gmail.com>
2020-03-29 01:48:37 +0100

    Upload args parser.

--- dcl.asm
+++ dcl.asm
@@ -5,40 +5,89 @@ MAX_ARG_LENGTH  equ 42
 
 global _start
 
-section .rodata
+section .bss
+
+Lperm: resb 8                   ; Address of L permutation on stack
+Rperm: resb 8                   ; Address of R permutation on stack
+Tperm: resb 8                   ; Address of T permutation on stack
+Linv:  resb MAX_ARG_LENGTH      ; Inversion of L permutation
+Rinv:  resb MAX_ARG_LENGTH      ; Inversion of R permutation
+buff:  resb 4096                ; Buffer for input-output
+Lbegin: resb 1
+Rbegin: resb 1
+
 
 section .text
+
+process_single_arg:                ; Sparsuj i zinvertuj jeden argument. rsi - source, rdi - target, r8 - ile liter ma byc
+    xor     rcx, rcx
+check_chars_loop:
+    cmp     rcx, r8                 ; całe słowo wczytane, teraz musi być 0
+    jl      check_single_char
+    cmp     byte[rsi + rcx], 0           ; NULL terminator, następny argument
+    jne     fail                   ; zbyt długie słowo
+    ret
+check_single_char:
+    xor     rax, rax ; może da się bez
+    mov     al, byte[rsi + rcx]
+    cmp     al, MIN_CHAR            ; czy w przedziale
+    jl      fail
+    cmp     al, MAX_CHAR
+    jg      fail
+    test    rdi, rdi                ; czy trzeba liczyć permutacje
+    jz      next_char               ; jesli nie, to nastepna literka
+    lea     rbx, [rdi + rax - MIN_CHAR] ; zaladuj adres na docelowej permutacji
+    cmp     byte[rbx], 0
+    jne     fail
+    mov     [rbx], eax
+next_char:
+    inc     rcx                    ; licznik++
+    jmp     check_chars_loop
 
 _start:
     cmp     QWORD[rsp], 5          ; sprawdz czy dobra liczba argumentow
     jne     fail                   ; niepoprawna liczba argumentow -> wyjdz z niezerowym
+    mov     r8, MAX_ARG_LENGTH
     lea     rbp, [rsp + 16]         ; adres args[1]
-check_args_loop:
     mov     rsi, [rbp]
-    test    rsi, rsi
-    jz      calc_inverse_perm      ; Napotkano zerowy wskaźnik, nie ma więcej argumentów.
-    xor     r8d, r8d               ; wyzeruj licznik pojedynczego argumentu
-check_chars_loop:
-    cmp     r8d, MAX_ARG_LENGTH    ; całe słowo wczytane, teraz musi być 0
-    jl      check_single_char
-    add     rbp, 8                 ; przejdz do nastepnego argumentu (rsi zostaje na starym)
-    cmp     rsi, 0                 ; NULL terminator, następny argument
-    je      check_args_loop
-    jmp     fail                   ; jesli nie skoczylismy poprzednim to znaczy że za długie
-check_single_char:
-    cmp     byte[rsi], MIN_CHAR          ; czy w przedziale
-    jl      fail
-    cmp     byte[rsi], MAX_CHAR
-    jg      fail
-    inc     rsi                    ; następna literka
-    inc     r8d                    ; licznik++
-    jnp     check_chars_loop
-calc_inverse_perm:
+    mov     [Lperm], rsi             ; zapisz adres permutacji L
+    mov     rdi, Linv
+    call    process_single_arg
+    add     rbp, 8
+    mov     rsi, [rbp]
+    mov     [Rperm], rsi             ; zapisz adres permutacji R
+    mov     rdi, Rinv
+    call    process_single_arg
+    add     rbp, 8
+    mov     rsi, [rbp]
+    xor     rdi, rdi               ; wyczysc rdi, bo nie potrzebujemy inwersji T
+    call    process_single_arg
+    xor     rcx, rcx                ; licznik
+check_T_permutation:                ; iterujemy sie po T i patrzymy czy poprawne zlozenie
+    cmp     rcx, MAX_ARG_LENGTH
+    je      check_key
+    xor     rax, rax
+    mov     al, byte[rsi + rcx]     ; na al jest literka
+    lea     r8, [rsi + rax - MIN_CHAR] ; zaladuj na r8 adres literki w permutacji
+    cmp     al, byte[r8]
+    je      fail                    ; punkt staly
+    mov     rdx, rcx                ; powinien byc wyzerowany
+    add     rdx, MIN_CHAR
+    cmp     dl, byte[rsi + rax - MIN_CHAR]
+    jne     fail                    ; nie ma dwuelementowego cyklu
+    inc     rcx
+    jmp     check_T_permutation
+check_key:
+    add     rbp, 8
+    mov     rsi, [rbp]
+    xor     rdi, rdi
+    mov     r8, 2                   ; ostatni argument ma tylko dwie literki
+    call    process_single_arg
 read_input:   
-    xor     edi, edi               ; wyczysc edi, exit z 0
+    xor     rdi, rdi               ; wyczysc edi, exit z 0
     jmp     exit
 fail:
-    mov     edi, 1          ; exit z codem 1
+    mov     edi, 1                 ; exit z codem 1
 exit:
     mov     rax, SYS_EXIT
     syscall
