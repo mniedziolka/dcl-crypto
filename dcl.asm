63f3780e8a3e400218c6d61bc1ba2727658fc9ec
niedziol <michalekniedziolka@gmail.com>
2020-03-30 05:34:42 +0200

    Upload faster version.

--- dcl.asm
+++ dcl.asm
@@ -1,7 +1,5 @@
 SYS_EXIT        equ 60
-SYS_READ        equ 0
 SYS_WRITE       equ 1
-STD_IN          equ 0
 STD_OUT         equ 1
 MIN_CHAR        equ '1' 
 MAX_CHAR        equ 'Z'
@@ -10,7 +8,7 @@ BUFF_SIZE       equ 4096
 POS_L           equ 'L' - MIN_CHAR
 POS_R           equ 'R' - MIN_CHAR
 POS_T           equ 'T' - MIN_CHAR
-POS_MAX         equ 42
+POS_MAX         equ MAX_CHAR - MIN_CHAR + 1
 
 global _start
 
@@ -21,181 +19,179 @@ Rperm: resb 8                   ; Address of R permutation on stack
 Tperm: resb 8                   ; Address of T permutation on stack
 Linv:  resb MAX_ARG_LENGTH      ; Inversion of L permutation
 Rinv:  resb MAX_ARG_LENGTH      ; Inversion of R permutation
-buff:  resb 4097                ; Buffer for input-output
+buff:  resb BUFF_SIZE + 1           ; Buffer for input-output
 
 
 section .text
 
 %macro modulo 0
-    cmp     eax, MAX_CHAR
-    jle     %%skip    
-    sub     eax, POS_MAX
-%%skip:
+    mov         ebx, esi
+    sub         ebx, POS_MAX
+    cmp         esi, MAX_CHAR
+    cmovg       esi, ebx
 %endmacro
 
 ; r12 - bemben R ; r10 - bemben L
 %macro apply_rot 0
-    inc     r12
-    cmp     r12, POS_MAX                       ; czy sie przekrecilo
-    jl     %%in_range
-    xor     r12, r12                           
+    inc         r12d
+    cmp         r12d, POS_MAX                       ; czy sie przekrecilo
+    jl          %%in_range
+    xor         r12d, r12d                          
 %%in_range:
-    cmp     r12, POS_L
-    je      %%rot_L    
-    cmp     r12, POS_R
-    je      %%rot_L
-    cmp     r12, POS_T
-    jne     %%skip
+    cmp         r12d, POS_L
+    je          %%rot_L    
+    cmp         r12d, POS_R
+    je          %%rot_L
+    cmp         r12d, POS_T
+    jne         %%skip
 %%rot_L:
-    inc     r10
-    cmp     r10, POS_MAX
-    jl     %%skip
-    xor     r10, r10
+    inc         r10d
+    cmp         r10d, POS_MAX
+    jl          %%skip
+    xor         r10d, r10d
 %%skip:
 %endmacro
 
-process_single_arg:                ; Sparsuj i zinvertuj jeden argument. rsi - source, rdi - target, r8 - ile liter ma byc
-    xor     rcx, rcx
-check_chars_loop:
-    cmp     rcx, r8                 ; całe słowo wczytane, teraz musi być 0
-    jl      check_single_char
-    cmp     byte[rsi + rcx], 0           ; NULL terminator, następny argument
-    jne     fail                   ; zbyt długie słowo
-    ret
-check_single_char:
-    movzx   rax, byte[rsi + rcx]
-    cmp     al, MIN_CHAR            ; czy w przedziale
-    jl      fail
-    cmp     al, MAX_CHAR
-    jg      fail
-    test    rdi, rdi                ; czy trzeba liczyć permutacje
-    jz      next_char               ; jesli nie, to nastepna literka
-    lea     rdx, [rdi + rax - MIN_CHAR] ; zaladuj adres na docelowej permutacji
-    cmp     byte[rdx], 0
-    jne     fail
-    mov     eax, ecx
-    add     eax, MIN_CHAR
-    mov     byte[rdx], al
-next_char:
-    inc     rcx                    ; licznik++
-    jmp     check_chars_loop
+; Rotejt, permutuj, cofnij. rsi - source(tu podmieniamy literke), rdi-permutacja do zaaplikowania, rdx - przesun i wroc o tyle
+%macro add_perm_sub 0
+    add         esi, edx
+    modulo
+    sub         esi, MIN_CHAR
+    movzx       esi, byte[rdi + rsi]
+    add         esi, POS_MAX
+    sub         esi, edx
+    modulo
+%endmacro
 
-add_perm_sub:                       ; Rotejt, permutuj, cofnij. rsi - source(tu podmieniamy literke), rdi-permutacja do zaaplikowania, rdx - przesun i wroc o tyle
-    movzx   rax, byte[rsi]
-    add     eax, edx
-    modulo
-    sub     eax, MIN_CHAR
-    movzx   rax, byte[rdi + rax]
-    add     eax, POS_MAX
-    sub     eax, edx
-    modulo
-    mov     byte[rsi], al
+process_single_arg:                ; Sparsuj i zinvertuj jeden argument. rsi - source, rdi - target, r8 - ile liter ma byc
+    xor         ecx, ecx
+.check_chars_loop:
+    cmp         ecx, r8d                 ; całe słowo wczytane, teraz musi być 0
+    jl          .check_single_char
+    cmp         byte[rsi + rcx], 0           ; NULL terminator, następny argument
+    jne         fail                   ; zbyt długie słowo
     ret
+.check_single_char:
+    movzx       eax, byte[rsi + rcx]
+    cmp         al, MIN_CHAR            ; czy w przedziale
+    jl          fail
+    cmp         al, MAX_CHAR
+    jg          fail
+    test        rdi, rdi                ; czy trzeba liczyć permutacje
+    jz          .next_char               ; jesli nie, to nastepna literka
+    lea         rdx, [rdi + rax - MIN_CHAR] ; zaladuj adres na docelowej permutacji
+    cmp         byte[rdx], 0
+    jne         fail
+    mov         eax, ecx
+    add         eax, MIN_CHAR
+    mov         byte[rdx], al
+.next_char:
+    inc         ecx                    ; licznik++
+    jmp         .check_chars_loop
 
 _start:
-    cmp     qword[rsp], 5          ; sprawdz czy dobra liczba argumentow
-    jne     fail                   ; niepoprawna liczba argumentow -> wyjdz z niezerowym
-    mov     r8, MAX_ARG_LENGTH
-    lea     rbp, [rsp + 16]         ; adres args[1]
-    mov     rsi, [rbp]
-    mov     [Lperm], rsi             ; zapisz adres permutacji L
-    mov     rdi, Linv
-    call    process_single_arg
-    add     rbp, 8
-    mov     rsi, [rbp]
-    mov     [Rperm], rsi             ; zapisz adres permutacji R
-    mov     rdi, Rinv
-    call    process_single_arg
-    add     rbp, 8
-    mov     rsi, [rbp]
-    mov     [Tperm], rsi             ; zapisz adres permutacji T
-    xor     rdi, rdi               ; wyczysc rdi, bo nie potrzebujemy inwersji T
-    call    process_single_arg
-    xor     rcx, rcx                ; licznik
-check_T_permutation:                ; iterujemy sie po T i patrzymy czy poprawne zlozenie
-    cmp     rcx, MAX_ARG_LENGTH
-    je      check_key
-    xor     rax, rax
-    mov     al, byte[rsi + rcx]     ; na al jest literka
-    lea     r8, [rsi + rax - MIN_CHAR] ; zaladuj na r8 adres literki w permutacji
-    cmp     al, byte[r8]
-    je      fail                    ; punkt staly
-    mov     rdx, rcx                ; powinien byc wyzerowany
-    add     rdx, MIN_CHAR
-    cmp     dl, byte[rsi + rax - MIN_CHAR]
-    jne     fail                    ; nie ma dwuelementowego cyklu
-    inc     rcx
-    jmp     check_T_permutation
-check_key:
-    add     rbp, 8
-    mov     rsi, [rbp]
-    xor     rdi, rdi
-    mov     r8, 2                   ; ostatni argument ma tylko dwie literki
-    call    process_single_arg
-    xor     r10, r10               
-    mov     r10b, byte[rsi]         ; L bembenek
-    sub     r10, MIN_CHAR
-    inc     rsi
-    xor     r12, r12
-check_r11:
-    mov     r12b, byte[rsi]
-    sub     r12, MIN_CHAR
-    inc     rsi
-    cmp     byte[rsi], 0            ; sprawdzamy czy klucz szyfrowania nie jest zbyt dlugi
-    jne     fail
-read_input:
-    xor     eax, eax
-    xor     edi, edi
-    mov     rsi, buff
-    mov     edx, BUFF_SIZE
+    cmp         qword[rsp], 5          ; sprawdz czy dobra liczba argumentow
+    jne         fail                   ; niepoprawna liczba argumentow -> wyjdz z niezerowym
+    mov         r8d, MAX_ARG_LENGTH
+    lea         rbp, [rsp + 16]         ; adres args[1]
+    mov         rsi, [rbp]
+    mov         [Lperm], rsi             ; zapisz adres permutacji L
+    mov         rdi, Linv
+    call        process_single_arg
+    add         rbp, 8
+    mov         rsi, [rbp]
+    mov         [Rperm], rsi             ; zapisz adres permutacji R
+    mov         rdi, Rinv
+    call        process_single_arg
+    add         rbp, 8
+    mov         rsi, [rbp]
+    mov         [Tperm], rsi             ; zapisz adres permutacji T
+    xor         edi, edi               ; wyczysc rdi, bo nie potrzebujemy inwersji T
+    call        process_single_arg
+    xor         ecx, ecx                ; licznik
+.check_T_permutation:                ; iterujemy sie po T i patrzymy czy poprawne zlozenie
+    cmp         ecx, MAX_ARG_LENGTH
+    je          .check_key
+    xor         eax, eax
+    mov         al, byte[rsi + rcx]     ; na al jest literka
+    lea         r8, [rsi + rax - MIN_CHAR] ; zaladuj na r8 adres literki w permutacji
+    cmp         al, byte[r8]
+    je          fail                    ; punkt staly
+    mov         edx, ecx                ; powinien byc wyzerowany
+    add         edx, MIN_CHAR
+    cmp         dl, byte[rsi + rax - MIN_CHAR]
+    jne         fail                    ; nie ma dwuelementowego cyklu
+    inc         ecx
+    jmp         .check_T_permutation
+.check_key:
+    add         rbp, 8
+    mov         rsi, [rbp]
+    xor         edi, edi
+    mov         r8d, 2                   ; ostatni argument ma tylko dwie literki
+    call        process_single_arg               
+    movzx       r10d, byte[rsi]         ; L bembenek
+    sub         r10d, MIN_CHAR
+    inc         rsi
+    movzx       r12d, byte[rsi]
+    sub         r12d, MIN_CHAR
+    inc         rsi
+    cmp         byte[rsi], 0            ; sprawdzamy czy klucz szyfrowania nie jest zbyt dlugi
+    jne         fail
+.read_input:
+    xor         eax, eax
+    xor         edi, edi
+    mov         rsi, buff
+    mov         edx, BUFF_SIZE
     syscall
-    mov     r11, rax
-    mov     rsi, buff
-    mov     byte[buff + r11], 0
-    mov     rdi, 0
-    mov     r8, r11
-    call    process_single_arg
-    xor     rcx, rcx                ; wyzeruj licznik
-encode_buffer:
+    mov         r11d, eax
+    mov         rsi, buff
+    mov         byte[buff + r11], 0
+    mov         edi, 0
+    mov         r8d, r11d
+    call        process_single_arg
+    xor         ecx, ecx                ; wyzeruj licznik
+.encode_buffer:
     apply_rot
 
-    lea     rsi, [buff + rcx]
+    movzx       esi, byte[buff + rcx]
 
-    mov     rdi, [Rperm]
-    mov     rdx, r12
-    call    add_perm_sub
+    mov         rdi, [Rperm]
+    mov         edx, r12d
+    add_perm_sub
 
-    mov     rdi, [Lperm]
-    mov     rdx, r10
-    call    add_perm_sub
+    mov         rdi, [Lperm]
+    mov         edx, r10d
+    add_perm_sub
 
-    mov     rdi, [Tperm]
-    xor     rdx, rdx 
-    call    add_perm_sub
+    mov         rdi, [Tperm]
+    xor         edx, edx 
+    add_perm_sub
 
-    mov     rdi, Linv
-    mov     rdx, r10
-    call    add_perm_sub
+    mov         rdi, Linv
+    mov         edx, r10d
+    add_perm_sub
 
-    mov     rdi, Rinv
-    mov     rdx, r12
-    call    add_perm_sub
+    mov         rdi, Rinv
+    mov         edx, r12d
+    add_perm_sub
 
-    inc     rcx
-    cmp     rcx, r11                ; jesli przejrzelismy cale wejscie to print
-    jl      encode_buffer
-print_output:
-    mov     eax, SYS_WRITE
-    mov     edi, STD_OUT
-    mov     rsi, buff
-    mov     rdx, r11
+    mov         byte[buff + rcx], sil
+
+    inc         ecx
+    cmp         ecx, r11d                ; jesli przejrzelismy cale wejscie to print
+    jl          .encode_buffer
+.print_output:
+    mov         eax, SYS_WRITE
+    mov         edi, STD_OUT
+    mov         rsi, buff
+    mov         edx, r11d
     syscall
-    cmp     rdx, BUFF_SIZE
-    jge     read_input
-    xor     rdi, rdi               ; wyczysc edi, exit z 0
-    jmp     exit
+    cmp         edx, BUFF_SIZE
+    jge         .read_input
+    xor         edi, edi               ; wyczysc edi, exit z 0
+    jmp         exit
 fail:
-    mov     edi, 1                 ; exit z codem 1
+    mov         edi, 1                 ; exit z codem 1
 exit:
-    mov     rax, SYS_EXIT
+    mov         eax, SYS_EXIT
     syscall
